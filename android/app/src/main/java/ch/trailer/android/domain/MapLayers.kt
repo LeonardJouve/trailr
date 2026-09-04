663d1b7cda64e383142ce92d954dada485798850
Léonard Jouve <leonard.jouve@gmail.com>
2026-09-04 20:19:14 +0200

    android: drop outdated MapLayers doc comment

--- android/app/src/main/java/ch/trailer/android/domain/MapLayers.kt
+++ android/app/src/main/java/ch/trailer/android/domain/MapLayers.kt
@@ -2,12 +2,6 @@ package ch.trailer.android.domain
 
 import ch.trailer.android.api.TourType
 
-/**
- * Map layer ids used by the swisstopo style.
- *
- * The satellite raster is always visible; each tour type overlays
- * its swisstopo trail network on top.
- */
 object MapLayers {
 
     fun overlayLayerId(type: TourType): String = when (type) {
